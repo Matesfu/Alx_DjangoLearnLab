f4ded78f790bacd476f52de96dc4a448e182035a
Tesfahun Kassahun <tesfu27kassahun@gmail.com>
2026-01-18 23:20:18 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.detail import CreateView
 from .models import Book
 from .models import Library
 
@@ -9,7 +13,7 @@ def list_books(request):
     List all books stored in the database.
     """
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'relationship_app/templates/relationship_app/list_books.html', {'books': books})
 
 #Class-Based View
 class LibraryDetailView(DetailView):
@@ -17,5 +21,22 @@ class LibraryDetailView(DetailView):
     Display details of a specific library and its books.
     """
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'relationship_app/templates/relationship_app/library_detail.html'
     context_object_name = 'library'
+
+#Login View
+
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/templates/relationship_app/login.html'
+
+#Logout View
+
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/templates/relationship_app/logout.html'
+
+#Registration View
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/templates/relationship_app/register.html'
+    success_url = reverse_lazy('login')
