db8bf0295da2f6c53140843f663f41ab80840b51
Tesfahun Kassahun <tesfu27kassahun@gmail.com>
2026-02-14 13:37:58 +0300

    third commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
@@ -13,6 +20,16 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    path('', views.home, name='home'),
-    path('posts/', views.posts, name='posts'),
+    # path('', views.home, name='home'),
+    # path('posts/', views.posts, name='posts'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('posts/', PostListView.as_view(), name='post-list'),
+
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
